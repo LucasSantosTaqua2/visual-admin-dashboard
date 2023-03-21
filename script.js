c5ed6eb31dfe0bf35f967a043153cffe498e0e77
Lucas Santos <lucassantosdev@hotmail.com>
2023-03-21 07:33:54 -0300

    v1.0

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ Orders.forEach(order => {
                             <td>${order.productName}</td>
                             <td>${order.productNumber}</td>
                             <td>${order.paymentStatus}</td>
-                            <td class="${order.shipping === 'Declined' ?  'danger' : order.shipping === 'Pending' ? 'warning' : 'success'}">${order.shipping}</td>
+                            <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'Pending' ? 'warning' : 'success'}">${order.shipping}</td>
                             <td class="primary">Details</td>
                             `;
     tr.innerHTML = trContent;
-    document.querySelector('table tbody').appendChild(tr);  
+    document.querySelector('table tbody').appendChild(tr);
 });
